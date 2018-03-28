105a71df94a2abe5c31a3d39f0f46ab3d4c565ba
USER <>
2018-03-28 23:43:09 +0900

    erbと見出し作成

--- tkg.rb
+++ tkg.rb
@@ -3,5 +3,18 @@ require 'sinatra/reloader'
 require 'erb'
 
 get '/tkg' do
-  erb ;lvo
+  erb :lv0
 end
+__END__
+@@lv0
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <mata charset="utf-8">
+        <title>Sinatra - paiza</title>
+        <style>body {padding: 30px;}</style>
+    </head>
+    <body>
+        <h1>LV0　卵かけご飯レベルの簡単さ</h1>
+    </body>
+</html>
