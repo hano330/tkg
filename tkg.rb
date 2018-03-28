815500bfab147028bb3c8563550a897f30defa79
USER <>
2018-03-28 23:54:24 +0900

    erbと分離した。

--- tkg.rb
+++ tkg.rb
@@ -5,16 +5,3 @@ require 'erb'
 get '/tkg' do
   erb :lv0
 end
-__END__
-@@lv0
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <mata charset="utf-8">
-        <title>Sinatra - paiza</title>
-        <style>body {padding: 30px;}</style>
-    </head>
-    <body>
-        <h1>LV0　卵かけご飯レベルの簡単さ</h1>
-    </body>
-</html>
